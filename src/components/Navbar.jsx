6011a39811ce7d42533bbb4ff22ecd7f59cd3a71
kubilay taşkafa <info.kubilay0@gmail.com>
2024-10-09 22:31:07 +0300

    Navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,51 @@
+import { FaSearch, FaShoppingBag, FaUser } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+
 const Navbar = () => {
-  return <div className="text-6xl text-red-600">Navbar</div>;
+  const navigate = useNavigate();
+  return (
+    <nav className="bg-white shadow-md ">
+      <div className="container mx-auto px-4 md:px-16 lg:px-24 py-4 flex justify-between items-center">
+        <div className="text-lg font-bold">
+          <h3 onClick={() => navigate("/")}>KUSE</h3>
+        </div>
+        <div className="relative flex-1 mx-4 ">
+          <form>
+            <input
+              type="text"
+              placeholder="Search"
+              className="w-full border py-2 px-4 outline-none"
+            />
+            <FaSearch className="absolute top-3 right-3 text-red-500" />
+          </form>
+        </div>
+        <div className="flex items-center space-x-4">
+          <FaShoppingBag
+            className="text-lg"
+            onClick={() => navigate("/card")}
+          />
+          <button className="hidden md:block">Login|Register</button>
+          <button className="block md:hidden">
+            <FaUser />
+          </button>
+        </div>
+      </div>
+      <div className="flex items-center justify-center space-x-10 py-4 text-sm font-bold">
+        <h6 className="hover:underline" onClick={() => navigate("/")}>
+          Home
+        </h6>
+        <h6 className="hover:underline" onClick={() => navigate("/shop")}>
+          Shop
+        </h6>
+        <h6 className="hover:underline" onClick={() => navigate("/contact")}>
+          Contact
+        </h6>
+        <h6 className="hover:underline" onClick={() => navigate("/about")}>
+          About
+        </h6>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
