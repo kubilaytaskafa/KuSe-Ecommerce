833673c39de29737e57c20c2366bc568f6a3eefe
kubilay taşkafa <info.kubilay0@gmail.com>
2024-10-10 17:17:03 +0300

    navbar-fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,11 @@ import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const navigate = useNavigate();
+
   return (
     <nav className="bg-white shadow-md ">
       <div className="container mx-auto px-4 md:px-16 lg:px-24 py-4 flex justify-between items-center">
-        <div className="text-lg font-bold">
+        <div className="text-lg font-bold cursor-pointer">
           <h3 onClick={() => navigate("/")}>KUSE</h3>
         </div>
         <div className="relative flex-1 mx-4 ">
@@ -19,28 +20,41 @@ const Navbar = () => {
             <FaSearch className="absolute top-3 right-3 text-red-500" />
           </form>
         </div>
+
         <div className="flex items-center space-x-4">
           <FaShoppingBag
-            className="text-lg"
+            className="text-lg cursor-pointer"
             onClick={() => navigate("/card")}
           />
           <button className="hidden md:block">Login|Register</button>
           <button className="block md:hidden">
-            <FaUser />
+            <FaUser className="cursor-pointer" />
           </button>
         </div>
       </div>
       <div className="flex items-center justify-center space-x-10 py-4 text-sm font-bold">
-        <h6 className="hover:underline" onClick={() => navigate("/")}>
+        <h6
+          className="hover:underline cursor-pointer"
+          onClick={() => navigate("/")}
+        >
           Home
         </h6>
-        <h6 className="hover:underline" onClick={() => navigate("/shop")}>
+        <h6
+          className="hover:underline cursor-pointer"
+          onClick={() => navigate("/shop")}
+        >
           Shop
         </h6>
-        <h6 className="hover:underline" onClick={() => navigate("/contact")}>
+        <h6
+          className="hover:underline cursor-pointer"
+          onClick={() => navigate("/contact")}
+        >
           Contact
         </h6>
-        <h6 className="hover:underline" onClick={() => navigate("/about")}>
+        <h6
+          className="hover:underline cursor-pointer"
+          onClick={() => navigate("/about")}
+        >
           About
         </h6>
       </div>
